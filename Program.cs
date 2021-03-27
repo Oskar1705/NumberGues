6a61ee97e7e002bd590caac2afc16bf2e22c6d0c
Oskar Przybysz <flamaster.vin@gmail.com>
2021-03-27 01:52:58 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumberGues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jan Paweł 2 ";
+            string name2 = "Gwałci małe dzieci ";
+            int godzina = 2137;
+
+
+            Console.WriteLine(name + name2 + godzina);
+        }
+    }
+}
